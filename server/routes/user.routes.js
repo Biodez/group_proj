51fd33e5591bade02bbedac08f47ab31acceb528
Afeez <aoladokun@my.apsu.edu>
2021-11-15 09:18:10 -0500

    backend files

---
+++ server/routes/user.routes.js
@@ -0,0 +1,12 @@
+const userController = require("../controllers/user.controller");
+const jwtMiddleware = require("../middleware/jwt.middleware");
+
+module.exports = (app) => {
+  app.post("/api/signup", userController.signUp);
+  app.post("/api/signin", userController.signIn);
+  app.get(
+    "/api/protected",
+    jwtMiddleware.authenticate,
+    userController.protected
+  );
+};
